4d06768a5d16b1df0bb2079bc41b553686e6a390
Nandakishor09 <nanda.v.kishor@gmail.com>
2026-07-22 11:43:12 +0530

    Update and rename bubbleSort_recursion.cpp to bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*void bubbleSort(vector<int> arr){
+
+    int n = arr.size();
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = 0; j <= i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}*/
+
 void bubbleSort(vector<int> &arr, int n){
     
     if(n == 1) return;
