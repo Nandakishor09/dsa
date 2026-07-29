75ab394327589d9eccbd3b6afdd34f8e8cac3bee
Nandakishor09 <nanda.v.kishor@gmail.com>
2026-07-29 22:44:39 +0530

    Update leetcode_485.cpp

--- leetcode_485.cpp
+++ leetcode_485.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Max Consecutive Ones
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
 int findMaxConsecutiveOnes(vector<int>& nums) {
         
     int maxi = 0;
