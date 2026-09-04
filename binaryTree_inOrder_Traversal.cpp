c00ad1572b48847b3483b0dbcea7a3f1b6348c81
Nandakishor <nanda.v.kishor@gmail.com>
2026-09-04 11:52:32 +0530

    initial commit

---
+++ binaryTree_inOrder_Traversal.cpp
@@ -0,0 +1,52 @@
+#include <bits./stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* createTree(vector<int> &arr, int i, int n){
+    if(i >= n)
+        return nullptr;
+
+    Node *root = new Node(arr[i]);
+
+    root->left = createTree(arr, 2 * i + 1, n);
+    root->right = createTree(arr, 2 * i + 2, n);
+
+    return root;
+}
+
+void inOrder(Node *root){
+    if(root == nullptr)
+        return;
+
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+int main(){
+    vector<int> arr = {10, 20, 30, 40, 50, 60, 70};
+
+    int n = arr.size();
+
+    Node *root = createTree(arr, 0, n);
+
+    inOrder(root);
+
+    return 0;
+
+}
