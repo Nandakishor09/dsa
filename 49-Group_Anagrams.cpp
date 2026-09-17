271ca64fcf34cc73d3209c4a33f4d512a137b92e
Nandakishor <nanda.v.kishor@gmail.com>
2026-09-18 00:22:44 +0530

    groupAanagrams() updated

--- 49-Group_Anagrams.cpp
+++ 49-Group_Anagrams.cpp
@@ -3,42 +3,33 @@ using namespace std;
 
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
     vector<vector<string>> ans;
-    for(int i = 0; i < strs.size(); i++){
-        int check = 0;
-        vector<char> chars;
+
+    for (int i = 0; i < strs.size(); i++) {
         vector<string> sett;
-        int cnt1 = 0;
-        /*if(strs[i].size() == 0){
-            sett.push_back(strs[i]);
-            ans.push_back(sett);
-            continue;
-        }*/
-        for(int j = 0; j < strs[i].size(); j++){
-            chars.push_back(strs[i][j]);
-            cnt1++;
+        sett.push_back(strs[i]);
+
+        vector<int> freq1(26, 0);
+        for (int j = 0; j < strs[i].size(); j++) {
+            freq1[strs[i][j] - 'a']++;
         }
-        for(int k = i + 1; k < strs.size(); k++){
-            int check = 0;
-            int cnt2 = 0;
-            for(int l = 0; l < strs[k].size(); l++){
-                for(int ii = 0; ii < chars.size(); ii++){
-                    if(strs[k][l] == chars[ii]){
-                        cnt2++;
-                        break;
-                    }
-                }
+
+        for (int k = i + 1; k < strs.size(); ) {
+            vector<int> freq2(26, 0);
+            for (int l = 0; l < strs[k].size(); l++) {
+                freq2[strs[k][l] - 'a']++;
             }
-            if(cnt1 == cnt2){
+
+            if (freq1 == freq2) {
                 sett.push_back(strs[k]);
                 strs.erase(strs.begin() + k);
-                check = 1;
             }
-        }
-        if(check == 0){
-            sett.push_back(strs[i]);
+            else {
+                 k++;
+            }
         }
         ans.push_back(sett);
     }
+
     return ans;
 }
 
