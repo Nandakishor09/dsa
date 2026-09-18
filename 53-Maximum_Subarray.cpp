8b901aaa0713fd627d34affa45b2d6aa39059313
Nandakishor <nanda.v.kishor@gmail.com>
2026-09-18 16:55:57 +0530

    maxSubArray() updated

--- 53-Maximum_Subarray.cpp
+++ 53-Maximum_Subarray.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int maxSubArray(vector<int>& nums) {
-    int maxi = 0;
-    int len = nums.size();
-    if(len == 1) return nums[0];
-    for(int i = 0; i < len; i++){
-        int sum = nums[i];
-        for(int j = i + 1; j < len; j++){
-            sum += nums[j];
-            maxi = max(maxi, sum);
-        }
-        maxi = max(maxi, sum);
+      
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+
+    for (int i = 1; i < nums.size(); i++) {
+        currentSum = max(nums[i], currentSum + nums[i]);
+        maxSum = max(maxSum, currentSum);
     }
-    return maxi;
+
+    return maxSum;
 }
 
 int main(){
-    vector<int> nums = {-2,1};
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout<<"Maximum Sum: "<<maxSubArray(nums)<<endl;
 
     return 0;
